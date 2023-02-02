bf35a217bee7153050e016abf9eae224f3b35391
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2023-02-02 14:42:01 +0000

    refactoring lang variant and burger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import './Header.css';
-import Navigation from '../Navigation/Navigation';
+import React from "react";
+import "./Header.css";
+import Navigation from "../Navigation/Navigation";
 
-function Header() {
+function Header(props) {
   return (
-    <header className='header'>
+    <header className="header">
       <Navigation />
-      {/* <div className='header__link-container'>
-        <a href='' className='header__link'>Home</a>
-        <a href='' className='header__link'>About me</a>
-        <a href='' className='header__link'>Skills</a>
-        <a href='' className='header__link'>Portfolio</a>
-        <a href='' className='header__link'>Contacts</a>
-      </div> */}
-      {/* <div className='header__menu'></div> */}
     </header>
   );
-};
+}
 
 export default Header;
-
