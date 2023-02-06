85b29b3a52a3efad3ffa29d0b98c5647ffa4a9bd
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2023-02-06 23:40:33 +0300

    fix hero!!

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./Hero.css";
+
 import { LangActiveContext } from "../../context/LangActiveContext";
+import "./Hero.css";
 
 function Hero() {
   const { lang } = React.useContext(LangActiveContext);
