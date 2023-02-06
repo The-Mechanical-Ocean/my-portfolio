8dea4d3e7b0b3416b2fed9b3c7cab00810d98812
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2023-02-06 13:49:47 +0000

    Update 12 files

---
+++ src/components/CardSkills/CardSkills.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./CardSkills.css";
+
+function CardSkills({ card }) {
+  const [isClick, setIsClick] = React.useState(false);
+
+  return (
+    <div className="card">
+      <div className="card__image-container">
+        <img className="card__image" src={card.image} alt="card image" />
+      </div>
+      <p className="card__text">{card.name}</p>
+      <div className="card__image-stars-container">
+        <img
+          className="card__image-stars"
+          src={card.imageStars}
+          alt="card image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CardSkills;
---
+++ src/utils/cards.js
@@ -0,0 +1,45 @@
+import imgHTML from "../images/HTML.png";
+import imgCSS from "../images/CSS.png";
+import imgJS from "../images/JS.png";
+import imgReact from "../images/React.png";
+import imgGit from "../images/Git.png";
+
+import imgHTMLStars from "../images/stars.png";
+import imgCSSStars from "../images/stars.png";
+import imgJSStars from "../images/stars.png";
+import imgReactStars from "../images/stars.png";
+import imgGitStars from "../images/stars.png";
+
+const cards = [
+  {
+    name: "HTML",
+    image: imgHTML,
+    imageStars: imgHTMLStars,
+    id: 1,
+  },
+  {
+    name: "CSS",
+    image: imgCSS,
+    imageStars: imgCSSStars,
+  },
+  {
+    name: "JS",
+    image: imgJS,
+    imageStars: imgJSStars,
+    id: 3,
+  },
+  {
+    name: "React",
+    image: imgReact,
+    imageStars: imgReactStars,
+    id: 4,
+  },
+  {
+    name: "Git",
+    image: imgGit,
+    imageStars: imgGitStars,
+    id: 5,
+  },
+];
+
+export default cards;
