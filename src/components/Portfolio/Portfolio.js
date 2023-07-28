6e575decc90bdd671b0ea2d554bfaec07213cf76
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2023-07-28 12:17:57 +0300

    portfolio grid add and fix png

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,6 +6,7 @@ import Portfolio2 from '../../images/portfolio2.png';
 import Portfolio3 from '../../images/portfolio3.png';
 import Portfolio4 from '../../images/portfolio4.png';
 import Portfolio5 from '../../images/portfolio5.png';
+import Portfolio6 from '../../images/portfolio6.png';
 
 function Portfolio() {
   const { lang } = React.useContext(LangActiveContext);
@@ -15,87 +16,135 @@ function Portfolio() {
     <section className="portfolio" id="portfolio">
       <h2 className="portfolio__title">{isRu ? 'Портфолио' : 'Portfolio'}</h2>
       <div className="portfolio__link-container">
-        <a
-          className="portfolio__image-link"
-          href="https://the-mechanical-ocean.github.io/how-to-learn/index.html"
-          target="blank"
-        >
-          <img className="portfolio__image" src={Portfolio2} alt="portfolio" />
-        </a>
-        <figcaption className="portfolio__figcaption">
+        <div className="portfolio__link-element">
           <a
-            className="portfolio__figcaption-link"
+            className="portfolio__image-link"
             href="https://the-mechanical-ocean.github.io/how-to-learn/index.html"
             target="blank"
           >
-            How To Learn
+            <img
+              className="portfolio__image"
+              src={Portfolio2}
+              alt="portfolio"
+            />
           </a>
-        </figcaption>
-        <a
-          className="portfolio__image-link"
-          href="https://the-mechanical-ocean.github.io/russian-travel/index.html"
-          target="blank"
-        >
-          <img className="portfolio__image" src={Portfolio1} alt="portfolio" />
-        </a>
-        <figcaption className="portfolio__figcaption">
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://the-mechanical-ocean.github.io/how-to-learn/index.html"
+              target="blank"
+            >
+              How To Learn
+            </a>
+          </figcaption>
+        </div>
+        <div className="portfolio__link-element">
           <a
-            className="portfolio__figcaption-link"
+            className="portfolio__image-link"
             href="https://the-mechanical-ocean.github.io/russian-travel/index.html"
             target="blank"
           >
-            Russian Travel
+            <img
+              className="portfolio__image"
+              src={Portfolio1}
+              alt="portfolio"
+            />
           </a>
-        </figcaption>
-        <a
-          className="portfolio__image-link"
-          href="https://the-mechanical-ocean.nomoredomains.sbs/sign-in"
-          target="blank"
-        >
-          <img className="portfolio__image" src={Portfolio4} alt="portfolio" />
-        </a>
-        <figcaption className="portfolio__figcaption">
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://the-mechanical-ocean.github.io/russian-travel/index.html"
+              target="blank"
+            >
+              Russian Travel
+            </a>
+          </figcaption>
+        </div>
+        <div className="portfolio__link-element">
           <a
-            className="portfolio__figcaption-link"
+            className="portfolio__image-link"
             href="https://the-mechanical-ocean.nomoredomains.sbs/sign-in"
             target="blank"
           >
-            Places
+            <img
+              className="portfolio__image"
+              src={Portfolio4}
+              alt="portfolio"
+            />
           </a>
-        </figcaption>
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://the-mechanical-ocean.nomoredomains.sbs/sign-in"
+              target="blank"
+            >
+              Places
+            </a>
+          </figcaption>
+        </div>
+        <div className="portfolio__link-element">
+          <a
+            className="portfolio__image-link"
+            href="https://t-m-o-portfolio-movies.nomoredomains.icu/"
+            target="blank"
+          >
+            <img
+              className="portfolio__image"
+              src={Portfolio5}
+              alt="portfolio"
+            />
+          </a>
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://t-m-o-portfolio-movies.nomoredomains.icu/"
+              target="blank"
+            >
+              Diploma project
+            </a>
+          </figcaption>
+        </div>
+        <div className="portfolio__link-element">
+          <a
+            className="portfolio__image-link"
+            href="https://the-mechanical-ocean.github.io/doctor-lending/"
+            target="blank"
+          >
+            <img
+              className="portfolio__image"
+              src={Portfolio3}
+              alt="portfolio"
+            />
+          </a>
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://the-mechanical-ocean.github.io/doctor-lending/"
+              target="blank"
+            >
+              Doctor Labunc
+            </a>
+          </figcaption>
+        </div>
+        <div className="portfolio__link-element">
+          <a className="portfolio__image-link" href="https://" target="blank">
+            <img
+              className="portfolio__image"
+              src={Portfolio6}
+              alt="portfolio"
+            />
+          </a>
+          <figcaption className="portfolio__figcaption">
+            <a
+              className="portfolio__figcaption-link"
+              href="https://"
+              target="blank"
+            >
+              http://???:)
+            </a>
+          </figcaption>
+        </div>
       </div>
-      <a
-        className="portfolio__image-link"
-        href="https://t-m-o-portfolio-movies.nomoredomains.icu/"
-        target="blank"
-      >
-        <img className="portfolio__image" src={Portfolio5} alt="portfolio" />
-      </a>
-      <figcaption className="portfolio__figcaption">
-        <a
-          className="portfolio__figcaption-link"
-          href="https://t-m-o-portfolio-movies.nomoredomains.icu/"
-          target="blank"
-        >
-          Diploma project
-        </a>
-      </figcaption>
-      <a
-        className="portfolio__image-link"
-        href="https://the-mechanical-ocean.github.io/doctor-lending/"
-        target="blank"
-      >
-        <img className="portfolio__image" src={Portfolio3} alt="portfolio" />
-      </a>
-      <figcaption className="portfolio__figcaption">
-        <a
-          className="portfolio__figcaption-link"
-          href="https://the-mechanical-ocean.github.io/doctor-lending/"
-          target="blank"
-        >
-          Doctor Labunc
-        </a>
-      </figcaption>
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './index.css';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import {createRoot} from "react-dom/client";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
 
